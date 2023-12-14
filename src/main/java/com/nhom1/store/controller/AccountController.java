0ff85bb23857113ccc4036be5d55777e94ad7426
NguyenMinhGiang6 <giangnguyen060102@gmail.com>
2023-12-15 00:03:52 +0700

    finish code Giang03

--- src/main/java/com/nhom1/store/controller/AccountController.java
+++ src/main/java/com/nhom1/store/controller/AccountController.java
@@ -68,6 +68,7 @@ public class AccountController {
 		model.addAttribute("user", user);
 		return "myAddress";
 	}
+	//Giang nhom03
 	
 	@RequestMapping(value="/update-user-address", method=RequestMethod.POST)
 	public String updateUserAddress(@ModelAttribute("address") Address address, 
